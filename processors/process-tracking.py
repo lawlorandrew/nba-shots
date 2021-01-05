10b950f0ea7fa6e4187e20a958efe1c3939a95d9
Andrew Lawlor <andrew.b.lawlor@vanderbilt.edu>
2021-01-04 18:27:27 -0600

    broke out home court advantage stuff into its own repo

--- processors/process-tracking.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas as pd  
-
-with open(f'./data/tracking/post_ups.json') as jsonFile:
-  data = json.load(jsonFile)
-  headers = data['resultSets'][0]['headers']
-  stats = data['resultSets'][0]['rowSet']
-  stats_df = pd.DataFrame(stats, columns=headers)
-  stats_df.to_csv(f'./data/tracking/post_ups.csv')
